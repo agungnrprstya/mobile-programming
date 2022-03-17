811596b65f661cf354d92fa9f253fff07d9db8a9
agungnrprstya <agungnurprasetya@gmail.com>
2022-03-17 17:46:52 +0700

    Menambah button

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
+      <View style={{marginTop:10}}>
+        <Button
+          title="Simpan"
+          color="#006400"
+          onPress={() => Alert.alert('Simple Button pressed')}
+          />
+      </View>
+      <View style={{marginTop:10}}>
+        <Button
+          title="Hapus"
+          color="#8b0000"
+          onPress={() => Alert.alert('Simple Button pressed')}
+          />
+      </View>
     </View>
   );
 }
