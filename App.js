b07c45a46cd010297cd52430c3f5e7a18f049708
agungnrprstya <agungnurprasetya@gmail.com>
2022-03-21 17:58:31 +0700

    Menambah Appbar, List, Button

--- App.js
+++ App.js
@@ -1,48 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, Image } from 'react-native';
+import * as React from 'react';
+import { Provider as PaperProvider, Appbar, DefaultTheme, List, Button } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'grey',
+    accent: '#f1c40f',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      {/* Menambahkan gambar */}
-        <Image
-        style={{width: 300, height: 300}}
-        source={{ 
-        uri: 'https://kompaspedia.kompas.id/wp-content/uploads/2021/07/logo-universitas-mercu-buana_thumb.png', }}/>
-
-      {/* Teks pada layar */}
-        <Text 
-        style={{ fontWeight: 'bold' }}
-        >Hello World!</Text>
-        <StatusBar 
-        style="auto" />
-      
-      {/* Tombol simpan */}
-      <View style={{marginTop:10}}></View>
-        <Button
-        title="Simpan"
-        color="#006400"
-        onPress={() => Alert.alert('Simple Button pressed')}
-        />
-      
-      {/* Tombol hapus */}
-      <View style={{marginTop:10}}></View>
-        <Button
-        title="Hapus"
-        color="#8b0000"
-        onPress={() => Alert.alert('Simple Button pressed')}
-        />
-
-    </View>
+    <PaperProvider theme={theme}>
+      <Appbar.Header>  
+        <Appbar.BackAction onPress={() => {}} />
+        <Appbar.Content title="Title" subtitle={'Subtitle'} />
+        <Appbar.Action icon="card-search-outline" onPress={() => {}} />
+        <Appbar.Action icon="dots-vertical-circle-outline" onPress={() => {}} />
+      </Appbar.Header>
+        <List.Item
+          title="First Item"
+          description="Item description"
+          left={props => <List.Icon {...props} icon="folder" />}/>
+        <Button icon="home" mode="contained" onPress={() => console.log('Pressed')}>Home</Button>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
