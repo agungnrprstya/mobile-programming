4ee327c53c2b6136a2a1e633419cb7f7b53bad9f
agungnrprstya <agungnurprasetya@gmail.com>
2022-03-31 13:50:19 +0700

    Belajar Properti dan Child

--- App.js
+++ App.js
@@ -1,119 +1,58 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import { Provider as PaperProvider, Appbar, DefaultTheme, List, Button, RadioButton, Text, Checkbox } from 'react-native-paper';
+import { Provider as PaperProvider, DefaultTheme, Appbar, Title, Button } from 'react-native-paper';
+import Child from './Child';
+import Usia from './Usia';
 
 const theme = {
   ...DefaultTheme,
   roundness: 2,
   colors: {
     ...DefaultTheme.colors,
-    primary: 'grey',
+    primary: 'blue',
     accent: '#f1c40f',
   },
 };
 
 export default function App() {
-  const [checked, setChecked] = React.useState(false, 'first');
-  const [checked2, setChecked2] = React.useState(false);
+  const [title, setTitle] = React.useState('Halo');
+  const [angka, setAngka] = React.useState(1);
   return (
     <PaperProvider theme={theme}>
-
-      {/* Appbar */}
-      <Appbar.Header>  
-        <Appbar.BackAction onPress={() => {}} />
-        <Appbar.Content title="Title" subtitle={'Subtitle'} />
-        <Appbar.Action icon="card-search-outline" onPress={() => {}} />
-        <Appbar.Action icon="dots-vertical-circle-outline" onPress={() => {}} />
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => setTitle('Halo')} />
+        <Appbar.Content title={title} subtitle={'Subtitle'} />
+        <Appbar.Action icon="pencil" onPress={() => setTitle('Pencil')} />
+        <Appbar.Action icon="delete" onPress={() => setTitle('Delete Icon')} />
       </Appbar.Header>
 
-      {/* List */}
-      <List.Item
-        title="First Item"
-        description="Item description"
-        left={props => <List.Icon {...props} icon="folder" />}/>
+      <Child 
+      judul="Belajar Component"
+      judul2="Kembali" 
+      labelButton="Tambah"
+      labelButton2="Home" />
       
-      {/* Checkbox */}
-      <View style={{ marginLeft:10, marginBottom:0 }}> 
-        <Text style={{ fontSize: 15 }}>Ini Checkbox</Text>
+      <Button 
+        icon="plus" 
+        mode="contained" 
+        style={{margin:20}} 
+        onPress={() => setAngka(angka+1)}>Tambah 
+      </Button>
+            
+      <View style={{alignItems:'center'}}>
+      <Title>{angka}</Title>
       </View>
 
-      <View style={{flexDirection:"row-reverse",alignItems:'center'}}>
-        <View style={{flex: 40}}>
-          <Text>Iya</Text>
-        </View>
-        <View style={{flex:1}}>
-          <Checkbox
-            status={checked ? 'checked' : 'unchecked'}
-            onPress={() => {setChecked(!checked);}}
-        />
-      </View>
-      </View>
+      <Button 
+        icon="minus" 
+        mode="contained"                    
+        style={{margin:20}}
+        onPress={() => setAngka(angka-1)}>Kurang
+      </Button>
 
-      <View style={{flexDirection:"row-reverse",alignItems:'center'}}>
-        <View style={{flex: 40}}>
-          <Text>Tidak</Text>
-        </View>
-        <View style={{flex:1}}>
-          <Checkbox
-            status={checked2 ? 'checked' : 'unchecked'}
-            onPress={() => {setChecked2(!checked2);}}
-          />
-      </View>
-      </View>
-       
-      {/* RadioButton */}
-      <View style={{ marginLeft:10, marginBottom:0, marginTop:20}}> 
-        <Text style={{ fontSize: 15 }}>Ini RadioButton</Text>
-      </View>
-      <View style={{flexDirection:"row-reverse",alignItems:'center'}}>
-        <View style={{flex: 40}}>
-          <Text>Iya</Text>
-        </View>
-        <View style={{flex:1}}>
-          <RadioButton.IOS
-            value="first"
-            status={ checked === 'first' ? 'checked' : 'unchecked' }
-            onPress={() => setChecked('first')}
-          />
-        </View>
-      </View>
-
-      <View style={{flexDirection:"row-reverse",alignItems:'center'}}>
-        <View style={{flex: 40}}>
-          <Text>Tidak</Text>
-        </View>
-        <View style={{flex:1}}>
-          <RadioButton.IOS
-            value="second"
-            status={ checked === 'second' ? 'checked' : 'unchecked' }
-            onPress={() => setChecked('second')}
-          />
-        </View>
-      </View>
-
-      {/* <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
-      <View style={{ marginLeft:19, marginBottom:-5 }}>
-        <Text style={{ fontSize: 20 }}>Apakah Saya Ganteng?</Text>
-      </View>
-
-      <View style={{ marginLeft:-7, marginBottom:-15 }}>
-        <RadioButton.Item
-          label="Iya" 
-          value="first" 
-          style={{ flexDirection: 'row-reverse', alignSelf: 'flex-start' }}/> 
-      </View>
-
-      <View style={{ marginLeft:-7 }}>
-        <RadioButton.Item
-          label="Tidak" 
-          value="second" 
-          style={{ flexDirection: 'row-reverse', alignSelf: 'flex-start' }} />
-      </View>
-      </RadioButton.Group> */}
-      
-      {/* Button */}
-      <Button icon="home" mode="contained" onPress={() => console.log('Pressed')}>Home</Button>
-
+      <Usia usia={angka}/>
+          
     </PaperProvider>
+
   );
 }
